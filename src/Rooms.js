cfda53932f147451a554a4e1775df40705a79c88
Trond Makonese <trondation@gmail.com>
2020-01-08 16:18:45 -0700

    Add room properties.

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,6 +1,10 @@
 class Rooms {
-  constructor() {
-
+  constructor(roomNumber, roomType, bedsize, numBeds, costPerNight) {
+    this.roomNumber = roomNumber;
+    this.roomType = roomType;
+    this.bedSize = bedSize;
+    this.numBeds = numBeds;
+    this.costPerNight = costPerNight;
   }
 }
 
