786e7870eba6974adf7ca0ee68fd61af00744bae
Trond Makonese <trondation@gmail.com>
2020-01-15 06:10:32 -0700

    transfer over data.

---
+++ data/bookings-data.js
@@ -0,0 +1,529 @@
+const bookingsData = [
+{
+id: 1572293130156,
+userID: 19,
+date: "2019/11/06",
+roomNumber: 18,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 16,
+date: "2019/11/06",
+roomNumber: 7,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 8,
+date: "2019/11/22",
+roomNumber: 1,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 3,
+date: "2019/11/22",
+roomNumber: 9,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 47,
+date: "2019/12/09",
+roomNumber: 14,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 20,
+date: "2019/12/01",
+roomNumber: 23,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 11,
+date: "2019/11/22",
+roomNumber: 8,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 23,
+date: "2019/12/07",
+roomNumber: 16,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 44,
+date: "2019/11/16",
+roomNumber: 12,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 7,
+date: "2019/11/01",
+roomNumber: 12,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 39,
+date: "2019/11/13",
+roomNumber: 21,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 46,
+date: "2019/12/03",
+roomNumber: 2,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 5,
+date: "2019/12/14",
+roomNumber: 11,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 45,
+date: "2019/11/10",
+roomNumber: 16,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 25,
+date: "2019/12/01",
+roomNumber: 9,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 30,
+date: "2019/11/06",
+roomNumber: 11,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 49,
+date: "2019/11/16",
+roomNumber: 5,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 18,
+date: "2019/12/16",
+roomNumber: 16,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 34,
+date: "2019/11/10",
+roomNumber: 12,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 31,
+date: "2019/11/17",
+roomNumber: 1,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 26,
+date: "2019/12/15",
+roomNumber: 22,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 10,
+date: "2019/11/24",
+roomNumber: 12,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130160,
+userID: 1,
+date: "2019/11/18",
+roomNumber: 5,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 9,
+date: "2019/11/07",
+roomNumber: 5,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 22,
+date: "2019/11/15",
+roomNumber: 15,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 35,
+date: "2019/12/06",
+roomNumber: 24,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 14,
+date: "2019/11/28",
+roomNumber: 23,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 28,
+date: "2019/12/06",
+roomNumber: 14,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 15,
+date: "2019/12/06",
+roomNumber: 5,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 48,
+date: "2019/11/14",
+roomNumber: 18,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 2,
+date: "2019/11/04",
+roomNumber: 7,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 17,
+date: "2019/11/04",
+roomNumber: 23,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 50,
+date: "2019/11/06",
+roomNumber: 14,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 6,
+date: "2019/11/20",
+roomNumber: 3,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 13,
+date: "2019/11/02",
+roomNumber: 22,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 33,
+date: "2019/12/08",
+roomNumber: 10,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 32,
+date: "2019/12/09",
+roomNumber: 15,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 37,
+date: "2019/11/07",
+roomNumber: 9,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 42,
+date: "2019/12/02",
+roomNumber: 9,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 4,
+date: "2019/11/14",
+roomNumber: 13,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 40,
+date: "2019/11/18",
+roomNumber: 25,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 24,
+date: "2019/11/09",
+roomNumber: 24,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 41,
+date: "2019/12/12",
+roomNumber: 24,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 36,
+date: "2019/11/30",
+roomNumber: 19,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 29,
+date: "2019/11/10",
+roomNumber: 2,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 43,
+date: "2019/12/13",
+roomNumber: 24,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 38,
+date: "2019/11/08",
+roomNumber: 10,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 41,
+date: "2019/12/07",
+roomNumber: 9,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 17,
+date: "2019/12/10",
+roomNumber: 24,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 37,
+date: "2019/11/15",
+roomNumber: 2,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 9,
+date: "2019/11/05",
+roomNumber: 20,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 30,
+date: "2019/11/29",
+roomNumber: 3,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 15,
+date: "2019/11/16",
+roomNumber: 25,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 27,
+date: "2019/12/08",
+roomNumber: 4,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130161,
+userID: 41,
+date: "2019/12/16",
+roomNumber: 15,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 24,
+date: "2019/12/10",
+roomNumber: 7,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 11,
+date: "2019/12/02",
+roomNumber: 11,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 34,
+date: "2019/11/26",
+roomNumber: 16,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 50,
+date: "2019/11/05",
+roomNumber: 18,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 31,
+date: "2019/11/15",
+roomNumber: 10,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 33,
+date: "2019/10/28",
+roomNumber: 8,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 16,
+date: "2019/11/23",
+roomNumber: 10,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 45,
+date: "2019/11/26",
+roomNumber: 23,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 7,
+date: "2019/11/13",
+roomNumber: 7,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 10,
+date: "2019/11/02",
+roomNumber: 20,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 5,
+date: "2019/11/02",
+roomNumber: 8,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 18,
+date: "2019/11/21",
+roomNumber: 7,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 16,
+date: "2019/11/25",
+roomNumber: 20,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 25,
+date: "2019/11/12",
+roomNumber: 12,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 47,
+date: "2019/11/03",
+roomNumber: 5,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 39,
+date: "2019/11/06",
+roomNumber: 21,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 38,
+date: "2019/12/01",
+roomNumber: 3,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 1,
+date: "2019/11/12",
+roomNumber: 21,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 3,
+date: "2019/11/28",
+roomNumber: 15,
+roomServiceCharges: [ ]
+},
+{
+id: 1572293130163,
+userID: 10,
+date: "2019/12/02",
+roomNumber: 18,
+roomServiceCharges: [ ]
+}
+];
+
+export default bookingsData;
